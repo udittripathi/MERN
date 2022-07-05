f74acb75c7f7f8aa09698786b9c0b4e0085219d8
udittripathi <71835574+udittripathi@users.noreply.github.com>
2022-07-05 21:15:48 +0530

    backend

--- router/auth.js
+++ router/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-require('../DB/conn');
 const User = require('../model/userSchema');
 
 router.get('/', (req, res) => {
@@ -19,16 +19,26 @@ router.post('/register', async (req, res) => {
     }
        
     try {
-        // if (password !== cpassword) {
-        //     return res.status(500).send({error: "Passwords mismatch", code: 500});
-        // }
-     //   const hash = await argon2.hash(password);
-        const newUser = new User({name, email, phone, work, password, cpassword});
-        await newUser.save();
+
+        const userExist = await User.findOne({ email: email});
+
+        if(userExist) {
+            return res.status(422).json({ error: "Email already Exist"});
+        }else if(password != cpassword) {
+            return res.status(422).json({ error: "password are not matching"});
+        }else {
+            const newUser = new User({name, email, phone, work, password, cpassword});
+
+            //         // hash the password
+            // const hashedPassword = await bcrypt.hash(password, 10);
+
+            await newUser.save();
         
-         return res.status(201).send({newUser});
-    //     const user = await newUser.save();
-    // res.status(200).json(user);
+            return res.status(201).send({newUser});
+        }
+  
+        
+
     } catch (err) {
         console.error(err);
         return res.status(500).send({error: err, code: 500});
@@ -36,5 +46,47 @@ router.post('/register', async (req, res) => {
 
 
 });
+
+router.post('/signin', async (req,res) => {
+    try{
+        let token;
+        const {email,password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({error: "Plz filled the data"})
+        }
+
+        const userLogin = await User.findOne({ email: email });
+
+          // console.log(userLogin);
+   
+        if(userLogin) {
+        const isMatch = await bcrypt.compare(password, userLogin.password);
+
+        token = await userLogin.generateAuthToken();
+        console.log(token);
+
+        res.cookie("jwtoken", token, {
+            expires: new Date(Date.now() + 25892000000),
+            httpOnly:true
+        });
+
+        if(!isMatch) {
+            res.status(400).json({message: "Invalid Credentials pass "});
+        } else {
+            res.json({message: "User Signin Succefully"});
+        }
+        }else {
+            res.status(400).json({message: "Invalid Credentials"});
+        }
+
+
+
+        
+    } catch(err) {
+        console.log(err);
+    }
+});
+
 
 module.exports = router;
